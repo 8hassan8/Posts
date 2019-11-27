981888f71ae4d7a4220a45139b0669dd9a352a0b
Hassan-Ali-GM <ha7899398@gmail.com>
2019-11-27 21:38:53 +0500

    Comments file created.

--- Post.js
+++ Post.js
@@ -7,11 +7,26 @@ myApp.posts = (() => {
       type: "Get",
 
       success: response => {
-        response.map(Element => {
+        response.map(async Post => {
+          let commentsHtml = "";
+          await getCommentsByPostId(Post.id)
+            .then(comments => {
+              comments.map(com => {
+                commentsHtml += `<h5>Email: ${com.email}  </h5> <p>Body: ${com.body}  </p>`;
+              });
+            })
+            .catch(error => {});
+
+          console.log(commentsHtml);
           const html = `<div style="background-color: whitesmoke;" class="mt-1">
-          <h5> Title: </h5>  ${Element.title}
-            <h6> Body: </h6>
-            ${Element.body}
+          <h5> Title: </h5>  ${Post.title}
+            <h6> Body: </h6> ${Post.body}
+            </div>
+            <div>
+            <h2> Comments: </h2>
+            <div class=mr-50>
+            ${commentsHtml}
+            </div>
           </div>
       `;
           $("#main-container").append(html);
@@ -23,7 +38,21 @@ myApp.posts = (() => {
     };
     $.ajax("https://jsonplaceholder.typicode.com/posts", settings);
   };
+  const getCommentsByPostId = postId => {
+    return new Promise((resolve, reject) => {
+      const settings = {
+        contentType: "application/json",
+        type: "Get",
+        success: response => resolve(response),
+        error: err => reject(err)
+      };
 
+      $.ajax(
+        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
+        settings
+      );
+    });
+  };
   const readyMain = () => {
     getAllPosts();
   };
